a96499796f33009177cce43bc88912d5de17d0e7
8enet <zlcn2200@yeah.net>
2015-09-22 19:14:37 +0800

    basic spider,auto obtain proxy address

---
+++ net_spider/main.py
@@ -0,0 +1,15 @@
+__author__ = 'zl'
+
+
+from net_spider.proxy_provider import WWW_xicidaili_com
+
+
+def main2():
+    wx = WWW_xicidaili_com()
+    res = wx.get_proxys(1, 2)
+    print(res)
+    pass
+
+if __name__ == '__main__':
+    main2()
+
